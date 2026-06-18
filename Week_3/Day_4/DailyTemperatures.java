72014dc34839cfc632c36f7b963356a9585094ce
Dipanshu Gandhi <dipanshugandhi9999@gmail.com>
2026-06-18 17:01:35 +0530

    Create DailyTemperatures.java

---
+++ Week_3/Day_4/DailyTemperatures.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack();
+        int[] ans = new int[temperatures.length];
+
+
+        for(int i = temperatures.length - 1; i >= 0; i--) {
+            int element = temperatures[i];
+
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= element) {
+                stack.pop();
+            }
+
+            if(stack.isEmpty()) {
+                ans[i] = 0;
+            }
+            else {
+                ans[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
