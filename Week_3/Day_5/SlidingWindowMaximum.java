0377bbe774a79a70fc0b87e6ab6ea304e62c5153
Dipanshu Gandhi <dipanshugandhi9999@gmail.com>
2026-06-19 17:08:43 +0530

    Create SlidingWindowMaximum.java

---
+++ Week_3/Day_5/SlidingWindowMaximum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int[] ans = new int[nums.length - k + 1];
+
+        for(int i = 0; i < k; i++) {
+            while(!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        ans[0] = nums[deque.peekFirst()];
+
+        for(int i = k; i < nums.length; i++) {
+            
+            if(deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+
+            while(!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+
+        return ans;
+    }
+}
